5e234afdf84b01f984b551a96557547fd3dd9ddb
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-06-18 18:53:15 +0300

    Base

--- src/index.js
+++ src/index.js
@@ -1,51 +1,113 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
 
 const ref = {
     searchForm: document.querySelector('.search-form'),
-    btn: document.querySelector('button'),
     gallery: document.querySelector('.gallery'),
+    btnLoadMore: document.querySelector('.load-more'),
 };
-const { searchForm, btn, gallery } = ref;
+const { searchForm, gallery, btnLoadMore } = ref;
+
+const URL = "https://pixabay.com/api/";
+const KEY = "37440122-e5d5a2493910548fa520b3add";
+const perPage = 40;
+let page = 1;
+let keyOfSearchPhoto = '';
+
+btnLoadMore.classList.add('is-hidden');
 
 searchForm.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(event) {
     event.preventDefault();
+    gallery.innerHTML = '';
+    page = 1;
     const { searchQuery } = event.currentTarget.elements;
-    const keyOfSearchPhoto = searchQuery.value
+    keyOfSearchPhoto = searchQuery.value
         .trim()
         .toLowerCase()
         .split(' ')
         .join('+');
     console.log(keyOfSearchPhoto);
 
-    fetchPhoto(keyOfSearchPhoto)
-    .then(data => {
-        console.log(data.hits);
-        const hits = data.hits;
-    })
-    .catch(onFetchError);
+    fetchPhoto(keyOfSearchPhoto, page)
+        .then(data => {
+            const searchResults = data.hits;
+            if (searchResults.length === 0) {
+                Notify.failure('Sorry, there are no images matching your search query. Please try again.', {
+                position: 'center-center',
+                timeout: 4000,
+                width: '400px',
+                fontSize: '24px'
+                });
+            };
+            console.log(searchResults);
+            createMarkup(searchResults);
+        })
+        .catch(onFetchError);
     
+    btnLoadMore.classList.remove('is-hidden');
+    btnLoadMore.addEventListener('click', onClickLoadMore);
+
     event.currentTarget.reset();
+};
+
+function onClickLoadMore() {
+    page += 1;
+    fetchPhoto(keyOfSearchPhoto, page)
+        .then(data => {
+            const numberOfPage = Math.ceil(data.totalHits / perPage);
+            const searchResults = data.hits;
+            console.log(searchResults);
+            createMarkup(searchResults);
+            if (page === numberOfPage) {
+                btnLoadMore.classList.add('is-hidden');
+                Notify.failure("We're sorry, but you've reached the end of search results.", {
+                    position: 'center-center',
+                    timeout: 4000,
+                    width: '400px',
+                    fontSize: '24px'
+                });
+            };
+        })
+        .catch(onFetchError);
 }
 
-const URL = "https://pixabay.com/api/";
-const KEY = "37440122-e5d5a2493910548fa520b3add";
+function createMarkup(searchResults) {
+    const arrPhotos = searchResults.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+        return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+            <b>Likes ${likes}</b>
+            </p>
+            <p class="info-item">
+            <b>Views ${views}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments ${comments}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads ${downloads}</b>
+            </p>
+        </div>
+        </div>`
+    });
+    gallery.insertAdjacentHTML("beforeend", arrPhotos.join(''));
+};
 
-function fetchPhoto(q) {
-    return fetch(`${URL}?key=${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        });       
+async function fetchPhoto(q, page) {
+    const response = await fetch(`${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`);
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json();             
 };
 
 function onFetchError() {
     Notify.failure('Oops! Something went wrong! Try reloading the page or make another choice!', {
         position: 'center-center',
-        timeout: 5000,
+        timeout: 4000,
         width: '400px',
         fontSize: '24px'
     });
